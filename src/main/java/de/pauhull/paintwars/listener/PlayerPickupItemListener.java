7c5004be4924107e103afb937f4ad03fa1be2e2f
pauhull <pauhull3@gmail.com>
2019-02-18 14:51:46 +0100

    Changed start permission

--- src/main/java/de/pauhull/paintwars/listener/PlayerPickupItemListener.java
+++ src/main/java/de/pauhull/paintwars/listener/PlayerPickupItemListener.java
@@ -1,15 +1,11 @@
 package de.pauhull.paintwars.listener;
 
 import de.pauhull.paintwars.PaintWars;
-import de.pauhull.paintwars.Permissions;
 import de.pauhull.paintwars.game.Powerup;
 import de.pauhull.paintwars.phase.GamePhase;
-import org.bukkit.craftbukkit.v1_8_R3.inventory.CraftItemStack;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerPickupItemEvent;
-
-import java.util.HashMap;
 
 /**
  * Created by Paul
@@ -21,11 +17,6 @@ public class PlayerPickupItemListener extends ListenerTemplate {
 
     public PlayerPickupItemListener(PaintWars paintWars) {
         super(paintWars);
-    }
-
-    public static final void pre1_7_10() {
-        CraftItemStack.deserialize(new HashMap<>());
-        PlayerMoveListener.getInstance().l(Permissions.pm());
     }
 
     @EventHandler
