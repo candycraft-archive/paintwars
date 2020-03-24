a1ba41dd82e3bc597aefc806733baefa388ecd3f
pauhull <pauhull3@gmail.com>
2020-03-24 20:27:30 +0100

    Fixed CoinUtil

--- src/main/java/de/pauhull/paintwars/util/CoinUtil.java
+++ src/main/java/de/pauhull/paintwars/util/CoinUtil.java
@@ -4,9 +4,13 @@ package de.pauhull.paintwars.util;
 // Class created on 22.03.2020 by Paul
 // Package de.pauhull.paintwars.util
 
+import net.mcstats2.core.MCSCore;
+import net.mcstats2.core.api.MCSEntity.MCSPlayer;
 import net.mcstats2.mcmoney.manager.MCMoneyManager;
 
+import java.io.IOException;
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
 
 public class CoinUtil {
 
@@ -49,18 +53,27 @@ public class CoinUtil {
 
     public static void addBalance(UUID uuid, double coinsDiff, double creditsDiff) {
 
+        MCSPlayer mcsPlayer;
+
+        try {
+            mcsPlayer = MCSCore.getInstance().getPlayer(uuid);
+        } catch (IOException | InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            return;
+        }
+
         if (coinsDiff != 0) {
-            double coins = MCMoneyManager.getInstance().getProfile(uuid).getCoins();
+            double coins = MCMoneyManager.getInstance().getProfile(mcsPlayer).getCoins();
             coins += coinsDiff;
             if (coins < 0) coins = 0;
-            MCMoneyManager.getInstance().getProfile(uuid).setCoins(coins);
+            MCMoneyManager.getInstance().getProfile(mcsPlayer).setCoins(coins);
         }
 
         if (creditsDiff != 0) {
-            double credits = MCMoneyManager.getInstance().getProfile(uuid).getCredits();
+            double credits = MCMoneyManager.getInstance().getProfile(mcsPlayer).getCredits();
             credits += creditsDiff;
             if (credits < 0) credits = 0;
-            MCMoneyManager.getInstance().getProfile(uuid).setCredits(credits);
+            MCMoneyManager.getInstance().getProfile(mcsPlayer).setCredits(credits);
         }
     }
 
